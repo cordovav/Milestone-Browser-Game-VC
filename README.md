fb0136702c7b477776aab22042f97e8ccc24b301
Veny Cordova <venycordova@gmail.com>
2022-10-22 11:58:59 -0400

    added psuedo code to javascript added code to hmtl and css

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Milestone-Browser-Game-VC
+# I will be building a Snake Game
+# The User will be able to control the snake around the screen to eat as many mice as possible
---
+++ index.js
@@ -0,0 +1,14 @@
+//Create the area were the snake will be moving
+
+//Create the snake
+
+//Add controls, eventlistners to the snake to have it more up, down, left, right
+
+//Give a command as to what happend when the snake eats something
+//Make the snake grow, and add on to the score
+
+//Create a win scenario
+
+//Create a if statement for when the user ends or loses the game
+//Without using console.log
+
