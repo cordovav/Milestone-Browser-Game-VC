4a7eafad9c6db8c7745f7386aa8abaa487b352c7
Veny Cordova <venycordova@gmail.com>
2022-10-26 18:10:19 -0400

    added more code to javascript, and readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Milestone-Browser-Game-VC
 # I will be building a Snake Game
 # The User will be able to control the snake around the screen to eat as many mice as possible
+# The user will have a set time limit
+# The user will be able to see there score of the amount of mice/food it has eaten
+# If the user runs of the screen it will result in a Game Over
--- index.js
+++ index.js
@@ -1,14 +1,105 @@
+//Assign all the elements being used
+const snakeGameBoard = document.querySelector(".gameboard")
+//one second interval
+const snakePosition = 
+const snakeVelocity = ;
+
+
+randomBait = document.querySelector('.rodent')
+
+function startGame(){
+
+}
+//Make the snake grow, and add on to the score
+function snakeMovements(x, y){
+    
+}
+//Create the snake
+function snake(){
+
+}
+
+
+
+
 //Create the area were the snake will be moving
 
-//Create the snake
-
 //Add controls, eventlistners to the snake to have it more up, down, left, right
+//use arrow keys
+function move(element){
+    element.style.position = 'fixed'
 
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+
+    }
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+
+        function moveSnake(){
+            if (direction === 'west'){
+                x-=1
+        }
+            if (direction === 'west'){
+                y+=1
+        }
+            if (direction === 'west'){
+                x+=1
+        }
+            if (direction === 'west'){
+                y-=1
+        }
+
+        element.syle.left = x + 'px'
+        element.syle.bottom = y + 'px'
+        }
+
+    setInterval(moveSnake,1)
+    document.addEventListener('keydown', function(e){
+        if(e.repeat)return;
+
+        if(e.key === 'ArrowLeft'){
+            direction = 'west'
+        }
+        if(e.key === 'ArrowUp'){
+            direction = 'north'
+        }
+        if(e.key === 'ArrowRight'){
+            direction = 'east'
+        }
+        if(e.key === 'ArrowDown'){
+            direction = 'south'
+        }
+        callback(direction)
+    })
+
+    document.addEventListener('keyup', function(e){
+            direction = null;
+            callback(direction)
+        })
+    }
+    return{
+        to: moveToCoordinates, 
+        withArrowKeys: moveWithArrowKeys,
+    }
+}
 //Give a command as to what happend when the snake eats something
-//Make the snake grow, and add on to the score
 
 //Create a win scenario
 
 //Create a if statement for when the user ends or loses the game
 //Without using console.log
+
+//set a time limit
+
+//set the score or how many rodents left to eat
+
+
+
 
